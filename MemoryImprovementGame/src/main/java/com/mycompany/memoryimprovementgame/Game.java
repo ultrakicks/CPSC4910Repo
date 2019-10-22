ead15934d8e605b63fb75929cc7cc98b163a6aa6
insomniac94 <insomniacnerd94@gmail.com>
2019-10-22 19:14:50 -0400

    Deleted dead code

--- MemoryImprovementGame/src/main/java/com/mycompany/memoryimprovementgame/Game.java
+++ MemoryImprovementGame/src/main/java/com/mycompany/memoryimprovementgame/Game.java
@@ -20,33 +20,31 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-
 /**
  *
  * @author matt
  */
 public class Game {
-    
+
     public static ArrayList<String> names = new ArrayList<>();
     public static ArrayList<String> relations = new ArrayList<>();
     public static ArrayList<String> filePaths = new ArrayList<>();
     public static ArrayList<JButton> images = new ArrayList<>();
-    
+
     public static void Main(String[] args) {
-        
-        
+
     }
-    
+
     public static void startGame() {
         //reads in names to an array
         try (InputStream input = new FileInputStream("namesconfig.properties")) {
             Properties propNames = new Properties();
-            
+
             //load the config file
             propNames.load(input);
-                       
+
             propNames.values().forEach(x -> names.add(x.toString()));
-            
+
             System.out.print("Names: " + names);
             System.out.println("");
         } catch (IOException ex) {
@@ -55,12 +53,12 @@ public class Game {
         //reads in relations to an array
         try (InputStream input = new FileInputStream("relationsconfig.properties")) {
             Properties propRelations = new Properties();
-            
+
             //load the config file
             propRelations.load(input);
-            
+
             propRelations.values().forEach(x -> relations.add(x.toString()));
-            
+
             System.out.print("Relations: " + relations);
             System.out.println("");
         } catch (IOException ex) {
@@ -69,68 +67,54 @@ public class Game {
         //reads in photo file paths to an array
         try (InputStream input = new FileInputStream("filepathconfig.properties")) {
             Properties propFilePaths = new Properties();
-            
+
             //load the config file
             propFilePaths.load(input);
-            
+
             propFilePaths.values().forEach(x -> filePaths.add(x.toString()));
-            
+
             System.out.print("File Paths: " + filePaths);
             System.out.println("");
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        
+
         for (int i = 0; i < filePaths.size(); i++) {
             images.add(new JButton());
         }
-        
+
         for (int i = 0; i < filePaths.size(); i++) {
             //ImageIcon icon = new ImageIcon(getClass().getResource(filePaths.get(i).toString()));
             images.get(i).setIcon(new ImageIcon(filePaths.get(i).toString()));
         }
-        
+
         GridLayout layout = new GridLayout(2, 3);
         JPanel picturePanel = new JPanel();
         for (int i = 0; i < images.size(); i++) {
             picturePanel.add(images.get(i));
         }
-         
-        /*
-        File imageTest = new File("D:\\wallpapers\\242_-_3sq9tqr.jpg");
-        ImageIcon testImg = new ImageIcon(imageTest);
-        JButton img = new JButton();
-        img.setIcon(testImg);
-        p.add(img);
-        */
-        
+
         // Get a random relation from the array list of relations
         Random rand = new Random();
         int randomInt = rand.nextInt(relations.size());
         String random_relation = relations.get(randomInt);
         System.out.println("Random Relation: " + random_relation);
-        
+
         // Add question prompt to a JPanel
         JPanel questionPanel = new JPanel();
         JLabel question = new JLabel("Click on your " + random_relation);
         question.setFont(new Font("Comic Sans", Font.PLAIN, 50));
         questionPanel.add(question);
-        
-        
+
         JFrame game = new JFrame();
         game.setPreferredSize(new Dimension(1200, 1000));
         game.pack();
         game.setLocationRelativeTo(null);
-        
+
         game.add(questionPanel);
-        //game.add(picturePanel);
-        
+        game.add(picturePanel);
+
         game.setVisible(true);
         System.out.println(filePaths);
-        
     }
-    
-    
 }
-
-
